35de250e60c5602332f0cc00452b55d93d863624
amstokely <andystokely@gmail.com>
2024-03-06 11:44:55 -0700

    Fixed python3.10 env support (hopefully).

--- check.py
+++ check.py
@@ -1,5 +1,11 @@
 def check():
-    return False
+    # randomly select 0 or 1
+    import random
+    result = random.choice([0, 1])
+    if result == 0:
+        return True
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     check()
